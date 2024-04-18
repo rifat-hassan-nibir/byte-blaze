ed3b40e0f3ccf324a8c33be7427361cbdd262bb4
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-04-18 19:29:20 +0600

    final push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,11 +20,6 @@ const Hero = () => {
               <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
               <span className="relative text-black group-hover:text-white">Read Blogs</span>
             </Link>
-            <Link to="/bookmarks" className="relative inline-block px-4 py-2 font-medium group">
-              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
-              <span className="relative text-black group-hover:text-white">Bookmarks</span>
-            </Link>
           </div>
         </div>
       </div>
